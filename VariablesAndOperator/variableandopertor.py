3061a4c7cba3c3003009239925c998e5a99bea83
Sandalanka <1415sandalanka@gmail.com>
2026-05-11 05:14:25 +0530

    feat: Variable and operators with python

---
+++ VariablesAndOperator/variableandopertor.py
@@ -0,0 +1,41 @@
+# String variable
+name = 'Isuru'
+
+print(name)
+
+# Integer variable and operator
+
+a = 20
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+# Floating variable and operator
+
+a = 20.5
+b = 2.4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+# Boolean variable
+
+t = True
+f = False
+
+age = 50
+
+isYoung = age < 18
+
+print(isYoung)
